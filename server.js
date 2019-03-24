d817d194d2c2979d1b5f74e461d16c3c5c6b1f64
Maria Ventosa <mariaventosa2@gmail.com>
2019-03-24 15:15:25 -0600

    session is saved with cookies, added logout, fixed home and login pages

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 const path = require('path');
-const app = express();
-
-const db = require('./queries')
 
 const Sequelize = require('sequelize');
+
+// const db = require('./queries')
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 var sequelize = new Sequelize('postgres://postgres@localhost:5432/auth-system');
 
 const User = sequelize.define('users', {
@@ -27,36 +36,95 @@ sequelize.sync()
     .then(() => console.log('users table has been successfully created, if one doesn\'t exist'))
     .catch(error => console.log('This error occured', error));
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/login.html'));
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'keyboardcat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
 });
 
+
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.redirect('/home');
+    } 
+    else {
+        next();
+    }    
+};
+
+
+app.get('/', sessionChecker, (req, res) => {
+    res.redirect('/login');
+});
+
+
+app.get('/login', sessionChecker, (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+
 app.post('/login', function (req, res) {
-	var username = req.body.username,
+  var username = req.body.username,
         password = req.body.password;
     console.log(username)
     console.log(password)
     User.findOne({ where: { username: username } }).then(function (user) {
             if (!user) {
                  res.redirect('/login');
+                 console.log('no user');
              }
-             
               else if (!user.validPassword(password)) {
+                console.log('wrong password');
                  res.redirect('/login');
              } 
-             else { res.sendFile(path.join(__dirname + '/home.html'));}
-
+             else { 
+              // res.sendFile(path.join(__dirname + '/home.html'));
+              console.log('access granted');
+              req.session.user = user.dataValues;
+                res.redirect('/home');
+             }
         });
-    // var name = req.body.username + ' ' + req.body.password;
-
+//     // var name = req.body.username + ' ' + req.body.password;
 });
 
-// app.get('/users', db.getUsers)
-// app.get('/users/:username', db.getUserByUsername)
+// route for user's dashboard
+app.get('/home', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.sendFile(__dirname + '/home.html');
+        console.log("going home");
+    } 
+    else {
+        res.redirect('/login');
+        console.log("back to login");
+    }
+});
+
+app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+        console.log("logged out");
+    } 
+    else {
+        res.redirect('/login');
+    }
+});
+
 
 const server = app.listen(3000, () => {
   console.log(`Express running on port ${server.address().port}`);
-});
-
+}); 
